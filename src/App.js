9b0800228853a7ee100d50213ee9232f86316413
qwerty-is-my-best-pass <qwerty.is.my.best.pass@gmail.com>
2021-03-11 22:27:10 +0500

    its alive!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import AwesomeComponent from '../test_sub_repo/AwesomeComponent';
+import AwesomeComponent from './common_sub/AwesomeComponent';
 
 function App() {
   return (
