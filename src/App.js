c5f1521b55ef469abac0bbb51d8eef9542b89ff1
qwerty-is-my-best-pass <qwerty.is.my.best.pass@gmail.com>
2021-03-11 22:25:59 +0500

    add and move submodule

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import AwesomeComponent from '../test_sub_repo/AwesomeComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Здесь скоро будет компонент
+        <AwesomeComponent text='А это второй репозиторий'/>
       </header>
     </div>
   );
